e3fc55765e4285e24f56228767b4997c575122bb
lucasmpramos <lucasmpr@me.com>
2025-02-02 23:50:54 -0300

    added comments to page template

--- src/components/page-template.tsx
+++ src/components/page-template.tsx
@@ -22,22 +22,27 @@ export default function PageTemplate({
 }: PageTemplateProps) {
   // If "centered", we can use a container class to center content.
   const containerClass =
-    layout === "centered" ? "mx-auto max-w-4xl p-4 pt-0" : "p-4 pt-0";
+    layout === "centered" 
+      ? "mx-auto max-w-4xl p-4 lg:px-8 pt-0" 
+      : "p-4 lg:px-8 pt-0";
 
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
+        {/* Header Top Bar */}
         <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <BreadcrumbNav items={[
+          <div className="flex items-center gap-2 px-4 lg:px-8">
+            <SidebarTrigger className="-ml-1" /> {/* Sidebar collapse button */}
+            <Separator orientation="vertical" className="mr-2 h-4" /> {/* Separator between sidebar trigger and breadcrumb */}
+            {/* Breadcrumb */}
+            <BreadcrumbNav items={[ 
               { title: "Platform", href: "#" },
               ...breadcrumbItems
             ]} />
           </div>
         </header>
+        {/* Page content container */}
         <div className={containerClass}>{children}</div>
       </SidebarInset>
     </SidebarProvider>
